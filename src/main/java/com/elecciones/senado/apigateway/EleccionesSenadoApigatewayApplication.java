6b3af8f18434ea034f941349da920e7a66ceee75
Jhonnathan0314 <jffc314@gmail.com>
2024-05-02 14:11:06 -0500

    Empty project

---
+++ src/main/java/com/elecciones/senado/apigateway/EleccionesSenadoApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.elecciones.senado.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleccionesSenadoApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleccionesSenadoApigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elecciones/senado/apigateway/EleccionesSenadoApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elecciones.senado.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleccionesSenadoApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
